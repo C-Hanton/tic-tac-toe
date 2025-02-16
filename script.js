b1f5b09ba4af3b933a9157d9d9625b62f84e483d
Conor_H <130234528+C-Hanton@users.noreply.github.com>
2025-02-16 15:55:20 +0000

    add 2d array to cycle through rows and columns toc reate gameboard

--- script.js
+++ script.js
@@ -4,4 +4,13 @@ function Gameboard() {
     const rows = 3;
     const columns = 3;
     const gameboard = [];
+
+    // Need a 2d array to create the game board for later
+    // i = 0 represents topmost row and j = 0 represents top left column
+    for(let i = 0; i < rows ; i++){
+        gameboard[i] = [];
+        for(let j = 0; j < columns ; j++){
+            board[i].push(Cell());
+        }
+    }
 }
