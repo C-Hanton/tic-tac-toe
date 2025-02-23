7198c49d1f9ea84a9654f1879df5f093e557ebfc
Conor_H <130234528+C-Hanton@users.noreply.github.com>
2025-02-23 15:10:29 +0000

    create the board

--- script.js
+++ script.js
@@ -14,4 +14,17 @@ const winningCombinations = [
     [0, 1, 2], [3, 4, 5], [6, 7, 8], //rows
     [0, 3, 6], [1, 4, 7], [2, 5, 8] // columns
     [0, 4, 8], [2, 4, 7] //diagonals
-]
+];
+//create the board
+function createBoard(){
+    board.innerHTML = ""; //clears the board
+    gameState.forEach((value, index) => {
+        const cell = document.createElement("div");
+        cell.classList.add("cell");
+        if(value) cell.classList.add("taken");
+        cell.dataset.index = index;
+        cell.textContent = value;
+        cell.addEventListener("click", handleCellClick);
+        board.appendChile(cell);
+    })
+}
