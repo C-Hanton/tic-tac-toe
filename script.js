88f71e770f25f6ffbd891c56780ac78a84c1701e
Conor_H <130234528+C-Hanton@users.noreply.github.com>
2025-02-23 15:01:22 +0000

    set up games essential variables

--- script.js
+++ script.js
@@ -3,3 +3,8 @@
 const board = document.getElementById("board");
 const statusText = document.getElementById("status");
 const resetButton = document.getElementById("reset");
+
+//set up games essential variables
+let currentPlayer = "X";
+let gameState = ["", "", "", "", "", "", "", "", ""]
+let gameActive = "true";
